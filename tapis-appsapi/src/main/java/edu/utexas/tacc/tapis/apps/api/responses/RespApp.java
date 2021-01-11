571ed0380f67449b21d287fb1573f0ea81ccf74d
Steve Black <scblack@tacc.utexas.edu>
2021-01-11 12:50:36 -0600

    Fix bug with client side json deserialization of notes attribute. Integ tests pass.

--- tapis-appsapi/src/main/java/edu/utexas/tacc/tapis/apps/api/responses/RespApp.java
+++ tapis-appsapi/src/main/java/edu/utexas/tacc/tapis/apps/api/responses/RespApp.java
@@ -1,15 +1,14 @@
 package edu.utexas.tacc.tapis.apps.api.responses;
 
+import edu.utexas.tacc.tapis.apps.api.responses.results.ResultApp;
 import edu.utexas.tacc.tapis.sharedapi.responses.RespAbstract;
 import edu.utexas.tacc.tapis.apps.model.App;
 
 public final class RespApp extends RespAbstract
 {
-  /**
-   * Zero arg constructor needed to use jersey's SelectableEntityFilteringFeature
-   */
-  public RespApp() { }
+  public ResultApp result;
 
-  public RespApp(App result) { this.result = result; }
-  public App result;
+  // Zero arg constructor needed to use jersey's SelectableEntityFilteringFeature
+  public RespApp() { }
+  public RespApp(App a) { result = new ResultApp(a); }
 }
