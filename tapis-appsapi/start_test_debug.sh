63c34ef173f1891fee8ab876918b31424a14a0ca
Steve Black <scblack@tacc.utexas.edu>
2021-02-12 18:17:30 -0600

    Rename jar artifact from v3#apps to apps

--- tapis-appsapi/start_test_debug.sh
+++ tapis-appsapi/start_test_debug.sh
@@ -11,4 +11,4 @@ export TAPIS_SVC_URL_TENANTS=https://dev.develop.tapis.io
 
 set -xv
 java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8081 \
-     -jar target/v3#apps.jar
+     -jar target/apps.jar
